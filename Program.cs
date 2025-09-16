89d136d683045f54e99487c8c9ae9a1ee0cd3c30
Franklin <ilamakovaleksej26@gmail.com>
2025-09-16 11:17:51 +0300

    Добавление меню выбора

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,42 @@ namespace ISIP523_Ilyamakov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Салам, мир!");
+
+            bool in_menu = true;
+            while (in_menu)
+            {
+                Console.WriteLine("Выберите пункт меню:");
+                Console.WriteLine("1. Вывод данных");
+                Console.WriteLine("2. Статистика");
+                Console.WriteLine("3. Сортировка по цене");
+                Console.WriteLine("4. Конвертация валюты");
+                Console.WriteLine("5. Поиск по названию");
+                Console.WriteLine("0. Выход");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        in_menu = false; 
+                        break;
+                    case 1:
+                        //Input();
+                        break;
+                    case 2:
+                        //GetStats();
+                        break;
+                    case 3:
+                        //Sorted;
+                        break;
+                    case 4:
+                        //ConvertC();
+                        break;
+                    case 5:
+                        //Search();
+                        break;
+                }
+                    
+            }
+
         }
     }
 }
