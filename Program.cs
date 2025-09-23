a3f8df9d039ee4f07ea12fae156ddb2acaa39d92
Franklin <ilamakovaleksej26@gmail.com>
2025-09-23 14:59:21 +0300

    Добален выбор количества добавления товаров

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -72,17 +73,22 @@ namespace ISIP523_Ilyamakov
 
         static void AddProduct()
         {
-            GID++;
-            Console.WriteLine("Название:");
-            string n = Console.ReadLine();
-            Console.WriteLine("Цена:");
-            double p = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Кол-во:");
-            int q = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Категория (1 - Молочные, 2 - Консервированые, 3 - Хлебобулочные");
-            int cn = Convert.ToInt32(Console.ReadLine());
-            Category c = (Category)cn;
-            products.Add(new Product(GID, n, p, q, c));
+            Console.WriteLine("Ведите кол-во товаров:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                GID++;
+                Console.WriteLine("Название:");
+                string nm = Console.ReadLine();
+                Console.WriteLine("Цена:");
+                double p = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Кол-во:");
+                int q = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Категория (1 - Молочные, 2 - Консервированые, 3 - Хлебобулочные");
+                int cn = Convert.ToInt32(Console.ReadLine());
+                Category c = (Category)cn;
+                products.Add(new Product(GID, nm, p, q, c));
+            }
         }
         static void SearchProduct()
         {
